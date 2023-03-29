f0a117abab34da452d6f0b97e6843b21bcb1ded4
LiamMosselmans <liam.mosselmans@student.ehb.be>
2023-03-29 11:55:59 +0200

    First push

---
+++ js/Circle.js
@@ -0,0 +1,42 @@
+import Figure from "./Figure.js";
+
+export default class Circle extends Figure {
+    constructor(name, radius) {
+        super(name);
+        this.radius = radius;
+    }
+    get diameter() {
+        return this.radius * 2;
+    }
+
+    get surfaceArea() {
+        return Math.PI * this.radius * this.radius;
+    }
+
+    get circumference() {
+        2 * Math.PI * this.radius;
+    }
+
+    get htmlString() {
+        const string = `<div class="result">
+        <div class="figure circle" style="width: DIAMETERpx; height: DIAMETERpx;"></div>
+        <div class="infoBox">
+          <h3>Circle</h3>
+          <dl>
+            <dt>Radius</dt>
+            <dd>RADIUS px</dd>
+      
+            <dt>Diameter</dt>
+            <dd>DIAMETER px</dd>
+      
+            <dt>Area</dt>
+            <dd>AREA px</dd>
+      
+            <dt>Circumference</dt>
+            <dd>CIRCUMFERENCE px</dd>
+          </dl>
+        </div>
+      </div>`
+      return string;
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,22 +1,30 @@
 "use strict";
-//import Figure from './Figure.js';
-//import Circle from './Circle.js';
-//import Rectangle from './Rectangle.js';
-//import Square from './Square.js';
+import Figure from './Figure.js';
+import Circle from './Circle.js';
+import Rectangle from './Rectangle.js';
+import Square from './Square.js';
 
 const form = {
     changeSelect(e) {
 
     },
     init() {
+        document.getElementById("select").addEventListener("change", this.changeSelect);
+        document.getElementById("form").addEventListener("submit", this.submitForm);
         console.log('Form init!');
     },
     inputElementsWithLabel(label, id) {
 
     },
     submitForm(e) {
+        e.preventDefault();
+        console.log("Form submitted!");
 
+        const rect = new Rectangle("unos", 50, 10);
+        console.log(rect.innerHtml);
+
+        document.querySelector('#results').innerHTML = rect.innerHtml;
     }
 };
 
-form.init();
+form.init();    
